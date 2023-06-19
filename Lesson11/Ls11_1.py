7bc0267bbc984d839c197aa0eedf620346399e36
Dmdenis <freewarrior82@gmail.com>
2023-06-19 19:42:10 +0300

    Изменения

--- Lesson11/Ls11_1.py
+++ Lesson11/Ls11_1.py
@@ -15,24 +15,29 @@ sbis_site = 'https://sbis.ru/'
 driver = webdriver.Chrome()
 try:
     driver.maximize_window()
+    print('открываем сайт sbis.ru')
     driver.get(sbis_site)
+    print('переходим в раздел контакты')
     contacts_btn = driver.find_element(By.CSS_SELECTOR, '.sbisru-Header__container [href="/contacts"]')
     contacts_btn.click()
     sleep(2)
-    banner_button = driver.find_element(By.CSS_SELECTOR, '[class="sbisru-Contacts__logo-tensor mb-8"]')
+    print('кликаем на баннер Тензор')
+    banner_button = driver.find_element(By.CSS_SELECTOR, '.sbis_ru-container [href="https://tensor.ru/"]')
     banner_button.click()
+    print('переключаемся на открытую вкладку')
     driver.switch_to.window(driver.window_handles[1])
+    print('Проверяем наличие блока сила в людях')
     power = driver.find_element(By.CSS_SELECTOR, '.tensor_ru-Index__block4-content .tensor_ru-Index__card-title')
     assert power.text == 'Сила в людях'
     assert power.is_displayed(), 'Новость "Сила в людях" не отображается'
     actions = ActionChains(driver)
-    sleep(2)
-    more = driver.find_element(By.CSS_SELECTOR, '[class="nl-LastCovers__header_newsLink"]')
+    print('кликаем на кнопку подробнее, проверяем что открылась страница https://tensor.ru/about')
+    more = driver.find_element(By.CSS_SELECTOR, '.tensor_ru-Index__block4-bg .tensor_ru-Index__block4-content')
     actions.move_to_element(more)
     actions.perform()
     about = driver.find_element(By.CSS_SELECTOR, '.tensor_ru-Index__block4-bg .tensor_ru-link')
     about.click()
-    assert driver.current_url == 'https://tensor.ru/about', 'Неверная адрес страницы'
+    assert driver.current_url == 'https://tensor.ru/about', 'Неверный адрес страницы'
     sleep(5)
 
 finally:
--- Lesson11/Ls11_2.py
+++ Lesson11/Ls11_2.py
@@ -18,47 +18,57 @@ user_password = 'кабинет6000'
 
 try:
     driver.maximize_window()
+    print('открываем страницу авторизации https://fix-online.sbis.ru/')
     driver.get(sbis_site)
     sleep(3)
+    print('вводим логин и пароль и авторизуемся')
     login = driver.find_element(By.CSS_SELECTOR, '[name="Login"]')
     login.send_keys(user_login, Keys.ENTER)
     password = driver.find_element(By.CSS_SELECTOR, '[name="Password"]')
     password.send_keys(user_password, Keys.ENTER)
     sleep(5)
+    print('открываем реестр контакты')
     contacts = driver.find_element(By.CSS_SELECTOR, '[data-qa="Контакты"]')
     contacts.click()
     sleep(2)
     dialogs = driver.find_element(By.CSS_SELECTOR, '.NavigationPanels-SubMenu__headTitle')
     dialogs.click()
     sleep(5)
+    print('нажимаем на кнопку плюс')
     add_contact = driver.find_element(By.CSS_SELECTOR, '[data-name="sabyPage-addButton"]')
     add_contact.click()
     sleep(5)
+    print('В строку поиска адрессата вводим данные')
     find_contact = driver.find_element(By.CSS_SELECTOR, '.addressee-selector-popup__browser '
                                                         '.controls-InputBase__nativeField_hideCustomPlaceholder')
     sleep(2)
 
     find_contact.send_keys('Василенко Вячеслав', Keys.ENTER)
     sleep(5)
+    print('Открываем окно нового сообщения адрессату')
     address = driver.find_element(By.CSS_SELECTOR, '[data-qa="person-Information__fio"]')
     address.click()
     sleep(5)
+    print('Вводим сообщение')
     message = driver.find_element(By.CSS_SELECTOR, '.textEditor_Viewer__Paragraph')
     message_txt = 'Cообщение'
     message.send_keys(message_txt)
+    print('Нажимаем отправить')
     send_message = driver.find_element(By.CSS_SELECTOR, '[title="Отправить"]')
     send_message.click()
     sleep(2)
+    print('Находим последнее сообщение, проверяем текст')
     new_message = driver.find_elements(By.CSS_SELECTOR, '.controls-BaseControl .msg-dialogs-item__message-text')[0]
     assert new_message.text == message_txt, 'Неверное сообщение'
     sleep(2)
+    print('удаляем сообщение')
     action = ActionChains(driver)
     action.move_to_element(new_message).perform()
     sleep(2)
     delete_button = driver.find_element(By.CSS_SELECTOR, '[data-qa="controls-itemActions__action deleteToArchive"]')
     delete_button.click()
     sleep(2)
-
+    print('Проверяем что удалилось')
     last_message = driver.find_elements(By.CSS_SELECTOR, '.controls-BaseControl .msg-dialogs-item__message-text')[0]
     assert last_message != message_txt
 finally:
